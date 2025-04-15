b1ddf9f1ae4583bc70bcc0ecf065215e7bf65393
Pradnesh Vedpathak <vedpathakpradnesh@gmail.com>
2025-04-15 13:17:42 +0530

    Tic-Tac-Toe(In progress)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import "./App.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { useState } from "react";
 
-function App() {
+function Square({ value, onSquareClick }) {
+  // const [value, setValue] = useState(null);
+
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick() {
+    setSquares("X");
+  }
   return (
     <>
-      <div>Ram Ram World!</div>
+      <div className="container">
+        <Square value={squares[0]} onSquareClick={() => handleClick} />
+        <Square value={squares[1]} onSquareClick={() => handleClick} />
+        <Square value={squares[2]} onSquareClick={() => handleClick} />
+
+        <Square value={squares[3]} onSquareClick={() => handleClick} />
+        <Square value={squares[4]} onSquareClick={() => handleClick} />
+        <Square value={squares[5]} onSquareClick={() => handleClick} />
+
+        <Square value={squares[6]} onSquareClick={() => handleClick} />
+        <Square value={squares[7]} onSquareClick={() => handleClick} />
+        <Square value={squares[8]} onSquareClick={() => handleClick} />
+      </div>
     </>
   );
 }
 
-export default App;
+export default Board;
