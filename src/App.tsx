0114e7b0adbcfb9b6c77f32fc16ae4a58feac468
Daksh Garade <daksh.garade.dg@gmail.com>
2025-04-15 22:18:04 +0530

    tic-tac-toe(In progress)

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,22 @@ function Square({ value, onSquareClick }) {
 }
 
 function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    console.log(xIsNext);
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
-    console.log(squares);
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
   return (
     <>
